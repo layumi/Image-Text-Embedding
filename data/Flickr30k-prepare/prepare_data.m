0a0b70f02e024b7d280712446f912e3ce61a815a
layumi <zdzheng12@gmail.com>
2017-11-22 22:59:25 +1100

    clear

--- data/Flickr30k-prepare/prepare_data.m
+++ data/Flickr30k-prepare/prepare_data.m
@@ -1,28 +1,12 @@
 addpath ./examples/imagenet;
-%load('./Flickr30k/flickr30k-txt.mat') ;
 load('./Flickr30k/train_val_test_split.mat') ;
-imdb.imageDir = './Flickr30k/flickr30k-images-400/';
-%load('./Flickr30k/rawdata-txt.mat');
-%norm 1   the hello has been normalized to 2.
-%hello = norm_zzd(hello);
-
-%norm 2
-%tmp = reshape(hello',4800,5,[]);
-%label = reshape(sum(tmp,2),4800,[]);
-%label = norm_zzd(label');
-
-%rank test
-%tt = hello(1,:);
-%score = hello*tt';
-%[~,rank] = sort(score,'descend');
+imdb.imageDir = './Flickr30k/flickr30k-images/';
 
 filename = dir([imdb.imageDir,'*jpg']);
 cell_data = {filename(1:end).name}.';
 full_path = cellfun(@(x) [imdb.imageDir,x],cell_data,'UniformOutput',false);
 imdb.images.data = full_path;
 imdb.images.set = set;
-%imdb.images.txt = raw_txt;
-%imdb.images.data2 = hello;
 
 count = 0;
 for i=1:numel(set)
@@ -38,11 +22,11 @@ end
 train = find(imdb.images.set == 1) ;
 images = fullfile(imdb.images.data(train(1:end))) ;
 [averageImage, rgbMean, rgbCovariance] = getImageStats(images, ...
-    'imageSize', [320 320], ...
+    'imageSize', [256 256], ...
     'numThreads', 16, ...
     'gpus', 3) ;
 %imdb.averageImage = averageImage;
 imdb.rgbMean = rgbMean;
 imdb.rgbCovariance =rgbCovariance;
 
-save('url_data_400.mat','imdb');
+save('url_data.mat','imdb');
