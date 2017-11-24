191c3624cb9f5672822eb715d833d01def28c466
Zhedong Zheng <zzheng@joo-ml1-dev.j.cinglevue.com>
2017-11-24 14:47:39 +0800

    add coco

--- coco_word2_Rankloss.m
+++ coco_word2_Rankloss.m
@@ -6,12 +6,12 @@ net = dagnn.DagNN.loadobj(netStruct.net) ;
 
 for i = 1:213 %img cnn
     if(mod(i,2)==0)
-        net.params(i).learningRate= 0; %0.02;
-    else net.params(i).learningRate= 0; %0.001;
+        net.params(i).learningRate= 0.02;
+    else net.params(i).learningRate= 0.001;
     end
-    net.params(i).weightDecay=0; %1;
+    net.params(i).weightDecay=1;
 end
-net.params(1).learningRate = 0; %0.0001;                                                                                                  
+net.params(1).learningRate = 0.0001;                                                                                                  
 
 net.addLayer('lrn1',dagnn.LRN('param',[4096,1e-8,1,0.5]),{'fc1_1bnx'},{'fc1bn_n'},{});
 net.addLayer('lrn2',dagnn.LRN('param',[4096,1e-8,1,0.5]),{'fc5_2bnx'},{'fc2bn_n'},{});
