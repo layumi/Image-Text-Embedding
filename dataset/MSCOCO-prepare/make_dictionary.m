88ae578bab029092f5761bb6581a6928ce833b1b
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2017-11-24 12:35:32 +1100

    Update make_dictionary.m

--- dataset/MSCOCO-prepare/make_dictionary.m
+++ dataset/MSCOCO-prepare/make_dictionary.m
@@ -1,5 +1,5 @@
-load('../GoogleNews_words.mat');
-load('../GoogleNews_vectors.mat');
+load('../../word2vector_matlab/GoogleNews_words.mat');
+load('../../word2vector_matlab/GoogleNews_vectors.mat');
 load('caption_train.mat');
 w_sum = cellfun(@(x) sum(x),w_names);
 subset.is_appear = zeros(1,3000000,'single');
