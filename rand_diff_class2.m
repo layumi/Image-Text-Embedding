a3f88684f56394d3e100b83a89e64b7b8fa91f2f
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2020-07-10 11:52:21 +1000

    Update rand_diff_class2.m

--- rand_diff_class2.m
+++ rand_diff_class2.m
@@ -2,7 +2,8 @@ function output = rand_diff_class2(imdb,label)
 %for cuhk-pede  txt diff
     index = find(imdb.images.label2~=label);
     output = index(randi(numel(index)));
-    while(imdb.images.label2(output)~=0)  %if val/test, sample again
+    %while(imdb.images.label2(output)~=0)  
+    while(imdb.images.label2(output)==0)  %if val/test, sample again
         output = index(randi(numel(index)));
     end
 end
